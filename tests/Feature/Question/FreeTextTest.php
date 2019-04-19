91f12821ea0b738742f95cac2bf8cdb69b4a50b3
james <yarbsemaj@gmail.com>
2019-04-19 11:05:11 +0100

    Added Free Text Question Text Question Test

---
+++ tests/Feature/Question/FreeTextTest.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Tests\Feature\Question;
+
+use App\Test;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class FreeTextTest extends TestCase
+{
+
+    private $user;
+
+    use RefreshDatabase;
+
+    public function setUp()
+    {
+        parent::setUp(); // I forgot this one
+
+        $this->user = factory('App\User')->create();
+    }
+
+    /**
+     * Adding the free text test.
+     *
+     * @return void
+     */
+    public function test_add_test()
+    {
+        $this->actingAs($this->user)
+            ->post(route('free-text.store'),
+                ['name' => 'example-name', 'description' => 'example-description'])
+            ->assertRedirect(route('free-text.index'));
+
+        $this->actingAs($this->user)
+            ->get(route('free-text.index'))
+            ->assertSeeText('example-name');
+
+    }
+
+    /**
+     * Adding the free text test.
+     *
+     * @return void
+     */
+    public function test_add_test_no_name()
+    {
+        $this->actingAs($this->user)
+            ->post(route('free-text.store'),
+                ['description' => 'example-description-no-name']);
+
+        $this->assertDatabaseMissing('tests', ['description' => 'example-description-no-name']);
+    }
+
+    /**
+     * Updating a free Test Test
+     *
+     * @return void
+     */
+    public function test_edit_test()
+    {
+        $this->actingAs($this->user)
+            ->post('free-text',
+                ['name' => 'example-name', 'description' => 'example-description'])
+            ->assertRedirect('free-text');
+
+        $test = Test::where('name', 'example-name')->where('user_id', $this->user->id)->first();
+
+        $this->actingAs($this->user)
+            ->put(route('free-text.update', ['id' => $test->id]),
+                ['name' => 'example-name-update', 'description' => 'example-description'])
+            ->assertRedirect(route('free-text.index'));
+
+        $this->actingAs($this->user)
+            ->get('free-text')
+            ->assertSeeText('example-name-update');
+
+    }
+
+    /**
+     * Updating a free Test Test
+     *
+     * @return void
+     */
+    public function test_remove_test()
+    {
+        $this->actingAs($this->user)
+            ->post('free-text',
+                ['name' => 'example-name', 'description' => 'example-description'])
+            ->assertRedirect('free-text');
+
+        $test = Test::where('name', 'example-name')->where('user_id', $this->user->id)->first();
+
+        $this->actingAs($this->user)
+            ->delete(route('free-text.destroy', ['id' => $test->id]))
+            ->assertRedirect(route('free-text.index'));
+
+        $this->actingAs($this->user)
+            ->get('free-text')
+            ->assertDontSeeText('example-name');
+
+    }
+}
